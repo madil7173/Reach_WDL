d8a0b3357f6b5b6add89240281b6411e596fde4b
madil7173 <madil7173@gmail.com>
2023-03-02 10:58:18 -0800

    updated grouped bed bash version

--- Reach_bash.sh
+++ Reach_bash.sh
@@ -1,7 +1,7 @@
 ml BEDTools/2.30.0-GCC-11.2.0
-cp /fh/fast/haffner_m/user/madil/scripts/Matrix_summary_generator.py ./
+cp /fh/fast/haffner_m/user/madil/scripts/MultiBed_Matrix_summary_generator.py ./
 bed_path="/fh/scratch/delete90/haffner_m/TFBS_beds/GTRD/hg19/beds_top10k/hg19LO_beds/"
-
+mkdir Summary_files
 for i in ${bed_path}*.bed;
 do
     bed_name=${i#"$bed_path"}
@@ -9,37 +9,26 @@ do
     sortBed -i $i > ${bed_name}.sorted.bed
 done
 ##generate slurm files
-bw_path="/fh/scratch/delete90/haffner_m/user/madil/WGBS_Atlas_GSE186458/bw/"
-for i in *.bed;
+bw_path="/fh/scratch/delete90/haffner_m/user/madil/Methylation_profiles/LuCaP/bw_files/"
+for j in ${bw_path}*bw;
 do
-    bed_name=${i%.sorted.bed}
-    #echo "${bed_name}"
-    touch ${bed_name}.slurm;
-    echo '#!/bin/bash' >> ${bed_name}.slurm;
-    echo '#SBATCH -c 4' >> ${bed_name}.slurm;
-    echo '#SBATCH -t 0-10:00' >> ${bed_name}.slurm;
-    echo '#SBATCH -o slurm.%j.out' >> ${bed_name}.slurm;
-    echo '#SBATCH -e slurm.%j.err' >> ${bed_name}.slurm;
-    echo '#SBATCH --mail-type=ALL' >> ${bed_name}.slurm;
-    echo '#SBATCH -A haffner_m' >> ${bed_name}.slurm;
-    echo '#SBATCH -p restart-new' >> ${bed_name}.slurm;
-    echo '#SBATCH -q restart-new' >> ${bed_name}.slurm;
-    echo "ml deepTools/3.5.1-foss-2021b" >> ${bed_name}.slurm;
-    echo "ml Python/3.9.6-GCCcore-11.2.0" >> ${bed_name}.slurm;
-    for j in ${bw_path}*bigwig; 
-    do
-        bw_name=${j#"$bw_path"}
-        bw_name=${bw_name%.bigwig}
-        spacer="_with_"
-        file_name="$bed_name$spacer$bw_name"
-        #echo "${bw_name}"
-        echo "${file_name}"
-        echo "computeMatrix reference-point -R $i -S $j -o ${file_name}.mtx.gz -b 1005 -a 1005 -bs 15 --referencePoint center -p=4 --outFileNameMatrix ${file_name}.values.tab --outFileSortedRegions  ${file_name}.used.bed" >> ${bed_name}.slurm;  
-        echo "python Matrix_summary_generator.py --input ${file_name}.values.tab --out ${file_name}.summary.txt" >> ${bed_name}.slurm;
-    done
-    #echo "wait" >> ${bed_name}.slurm;
-    #echo "cat *.summary.txt > ${out_put_matrix_name}.values.summary.txt" >> ${bed_name}.slurm;
+    bw_name=${j#"$bw_path"}
+    bw_name=${bw_name%.bw}
+    touch ${bw_name}.slurm;
+    echo '#!/bin/bash' >> ${bw_name}.slurm;
+    echo '#SBATCH -c 2' >> ${bw_name}.slurm;
+    echo '#SBATCH -t 0-30:00' >> ${bw_name}.slurm;
+    echo '#SBATCH -o slurm.%j.out' >> ${bw_name}.slurm;
+    echo '#SBATCH -e slurm.%j.err' >> ${bw_name}.slurm;
+    echo '#SBATCH --mail-type=ALL' >> ${bw_name}.slurm;
+    echo '#SBATCH -A haffner_m' >> ${bw_name}.slurm;
+    echo "ml deepTools/3.5.1-foss-2021b" >> ${bw_name}.slurm;
+    echo "ml Python/3.9.6-GCCcore-11.2.0" >> ${bw_name}.slurm;
+    #echo "${bw_name}"
+    echo "${bw_name}"
+    echo "computeMatrix reference-point -R *.sorted.bed -S $j -o ${bw_name}.mtx.gz -b 1005 -a 1005 -bs 15 --referencePoint center -p=20 --outFileNameMatrix ${bw_name}.values.tab --outFileSortedRegions  ${bw_name}.used.bed" >> ${bw_name}.slurm;  
+    echo "python MultiBed_Matrix_summary_generator.py --values_file ${bw_name}.values.tab --out ./Summary_files/" >> ${bw_name}.slurm;  
 done
 
-out_put_matrix_name="WGBS_Atlas_GTRD_top10"
+out_put_matrix_name="EPIC_LUCAP_GTRD_top10k_hg19"
 cat *.summary.txt > ${out_put_matrix_name}.values.summary.txt
